b6eb8ceb946042fc8217216b737149589a3b9322
Donald Plummer <donald@cideasphere.com>
2012-02-24 10:51:27 -0800

    Use setex to set expiry with one call

--- lib/redis-session-store.rb
+++ lib/redis-session-store.rb
@@ -35,7 +35,7 @@ class RedisSessionStore < ActionController::Session::AbstractStore
     def prefixed(sid)
       "#{@default_options[:key_prefix]}#{sid}"
     end
-    
+
     def get_session(env, sid)
       sid ||= generate_sid
       begin
@@ -50,15 +50,17 @@ class RedisSessionStore < ActionController::Session::AbstractStore
     def set_session(env, sid, session_data)
       options = env['rack.session.options']
       expiry  = options[:expire_after] || nil
-      
+
       @redis.pipelined do
-        @redis.set(prefixed(sid), Marshal.dump(session_data))
-        @redis.expire(prefixed(sid), expiry) if expiry
+        if expiry
+          @redis.setex(prefixed(sid), expiry, Marshal.dump(session_data))
+        else
+          @redis.set(prefixed(sid), Marshal.dump(session_data))
+        end
       end
-        
+
       return true
     rescue Errno::ECONNREFUSED
       return false
     end
-  
 end
