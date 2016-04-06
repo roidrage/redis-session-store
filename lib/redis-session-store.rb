5e207df155540302d5de05b9b65a88bfa4d0f6b6
Igor Rzegocki <ajgon@irgon.com>
2016-04-06 16:53:10 +0200

    Added support for Rack 2.0 `delete_session`. Fixes #68

--- lib/redis-session-store.rb
+++ lib/redis-session-store.rb
@@ -140,6 +140,7 @@ class RedisSessionStore < ActionDispatch::Session::AbstractStore
   def destroy_session(env, sid, options)
     destroy_session_from_sid(sid, (options || {}).to_hash.merge(env: env))
   end
+  alias_method :delete_session, :destroy_session
 
   def destroy(env)
     if env['rack.request.cookie_hash'] &&
